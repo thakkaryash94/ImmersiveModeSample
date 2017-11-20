4df37b834eacbc38fe4df158decd42c387a9ec2a
Yash Thakkar <thakkaryash94@gmail.com>
2017-11-21 00:45:06 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Android Immersive Mode Sample
+Android 4.4 Immersive Mode Toolbar Port
+
+This is direct port from Holo theme to Material Design Toolbar.
+
+With Android 4.4(API Level 19) KitKat, your apps can now truly go full-screen with a new Immersive Mode. Immersive Mode lets your apps hide the system's status and navigation bars while capturing all touch events—ideal for rich interactive content such as books and games. This video demonstrates how to use the new API, in addition to recapping earlier full-screen APIs on Android.
+
+Developer Website Link: https://developer.android.com/training/system-ui/immersive.html
+
+Video Link: https://www.youtube.com/watch?v=cBi8fjv90E4
+
+Original Sample Code: https://commondatastorage.googleapis.com/androiddevelopers/shareables/Fdevbytes/ImmersiveMode.zip
+
+### Special Thanks
+* Android Team
+* Roman Nurik
---
+++ app/src/main/java/example/yash/immersivemodesample/ImmersiveStickyActivity.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example.yash.immersivemodesample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class ImmersiveStickyActivity extends AppCompatActivity {
+    private View mDecorView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.immersive_sticky_activity);
+        mDecorView = getWindow().getDecorView();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+}
